bac437ad6dd3f0d8a483f63db4b9b467871b9c96
Alexander Peshkov <alexander.peshkov27@gmail.com>
2020-12-08 23:15:14 +0300

    Русский яз

--- src/main/kotlin/mainKt.kt
+++ src/main/kotlin/mainKt.kt
@@ -4,19 +4,19 @@ import java.util.Scanner
 fun main() {
 
     val scanner = Scanner(System.`in`)
-    println("Скольким людям понравилось LIKE:")
+    println("РљРѕР»РёС‡РµСЃС‚РІРѕ LIKE:")
     val likes = scanner.nextInt()
     val result = StringBuilder()
         .append(likes)
         .append(" ")
         .append(likesPostFix(likes))
-    println("Понравилось $result")
+    println("РџРѕРЅСЂР°РІРёР»РѕСЃСЊ $result")
 }
 
 fun likesPostFix(likes : Int): String {
     return if (likes % 2 == 0 || likes.toString().endsWith("1")) {
-        return "людям"
+        return "Р»СЋРґСЏРј"
     } else {
-        return "человеку"
+        return "С‡РµР»РѕРІРµРєСѓ"
     }
 }
